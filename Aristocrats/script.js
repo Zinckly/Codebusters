28e7ed0cbd49b24d98a31dab2fb3e4771731a179
Zinckly <aditya.adapa18@outlook.com>
2025-07-01 21:28:41 -0500

    Added ciphertext generator to aristocrat

--- Aristocrats/script.js
+++ Aristocrats/script.js
@@ -1,4 +1,6 @@
 function scramble(arr) {
+    arr = arr.slice(); // Create a copy of the array to avoid modifying the original
+    console.log("scrambling");
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -6,25 +8,32 @@ function scramble(arr) {
     return arr;
 }
 
-var quote = "";
-var author = "";
-fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(data => {
+var quote;
+var author;
+async function fetchQuote() {
+    try {
+        const response = await fetch('https://api.quotable.io/random');
+        const data = await response.json();
         console.log(`"${data.content}" — ${data.author}`);
         quote = data.content.toUpperCase();
         author = data.author;
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Error fetching quote:', error);
         alert("Error fetching quote. Try going to https://api.quotable.io/random directly in your browser and come back again.");
-    });
-
-var length = quote.length;
-
-var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var cipherText = scramble(alphabet);
-
-for (var i = 0; i < length; i++) {
-    document.getElementById("quoteBox").innerHTML += "<div class = 'cipherText'><p>" + cipherText[alphabet.indexOf(quote[i])] + "</p><button>H</button></div>";
+    }
 }
+fetchQuote().then(() => {
+
+    var ee = quote.length;
+
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var cipherText = scramble(alphabet);
+
+    for (var i = 0; i < ee; i++) {
+        if (quote[i] === " ") {
+            document.getElementById("quoteBox").innerHTML += "<div class = 'cipherText'><p> </p><button>H</button></div>";
+            continue;
+        }
+        document.getElementById("quoteBox").innerHTML += "<div class = 'cipherText'><p>" + cipherText[alphabet.indexOf(quote[i])] + "</p><button>H</button></div>";
+    }
+});
